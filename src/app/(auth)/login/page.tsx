05a200277373cb270989ca4b4897b2a6bf54b15f
Tom Keddie <thomaskeddie@gmail.com>
2025-09-10 08:43:22 +0000

    [09:40:06.679] Running build in Washington, D.C., USA (East) – iad1 [09:

--- src/app/(auth)/login/page.tsx
+++
@@ -1,7 +0,0 @@
-
-import { redirect } from 'next/navigation';
-
-// This page is no longer used since user authentication has been removed.
-export default function LoginPage() {
-  redirect('/dashboard');
-}
