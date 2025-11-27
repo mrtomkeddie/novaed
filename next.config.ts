5e57d022be5d6e6940b5ce1316b863b31d0623c6
mrtomkeddie <141493078+mrtomkeddie@users.noreply.github.com>
2025-11-27 12:14:41 +0000

    Suppress Handlebars require.extensions warning: externalize server-only, ignore warning

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || []
+      config.externals.push({ handlebars: 'commonjs handlebars' })
+      config.ignoreWarnings = config.ignoreWarnings || []
+      config.ignoreWarnings.push(/require\.extensions is not supported by webpack/)
+    }
+    return config
+  },
 };
 
 export default nextConfig;
